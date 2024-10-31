be1eb6afb74ca5dcea672fc405f7470400491027
Смирнова Галина <bgalina86@gmail.com>
2024-10-31 16:40:42 +0300

    Start Location and Page
    Signed-off-by: Смирнова Галина <bgalina86@gmail.com>

--- src/test/java/com/example/inno_certification4/saucedemo/pom/Authorization.java
+++
@@ -1,5 +0,0 @@
-package com.example.inno_certification4.saucedemo.pom;
-
-public class Authorization {
-
-}
--- src/test/java/com/example/inno_certification4/saucedemo/pom/pom/pages/AuthorizationPage.java
+++
@@ -1,5 +0,0 @@
-package com.example.inno_certification4.saucedemo.pom.pom.pages;
-
-public class AuthorizationPage {
-
-}
